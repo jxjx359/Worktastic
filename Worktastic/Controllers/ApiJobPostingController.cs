3f0aeaa589e1e7bc441aacae93fde9cf893782f9
jxjx359 <jonashofacker9@gmail.com>
2024-06-04 17:33:19 +0200

    RESTful API: GetAll Endpoint (GET)

---
+++ Worktastic/Controllers/ApiJobPostingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Worktastic.Data;
+
+namespace Worktastic.Controllers
+{
+    [Route("api/jobposting")]
+    [ApiController]
+    public class ApiJobPostingController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ApiJobPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var allJobPostings = _context.JobPostings.ToArray();
+            return Ok(allJobPostings);
+        }
+    }
+}
